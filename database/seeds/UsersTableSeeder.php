e4b221065953b6371ab93c32a2fa4edb60cd54bc
Nikol <nikolagavr@gmail.com>
2018-10-21 21:24:37 +0200

    Till chat, auth finished

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user1 = [
+          'name' => 'Nikola Gavrovski',
+          'email' => 'nikolagavr@gmail.com',
+          'password' => Hash::make('password'),
+        ];
+
+        User::create($user1);
+
+    }
+}
